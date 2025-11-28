e690add8b6f4dbc265bdbfc363ce9db9a4b5d3cc
Opilane TTHK <opilane@tthk.ee>
2025-11-28 13:47:15 +0200

    eelarve arvutaja algus

---
+++ Method2/Method2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Method2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Tervitus();
+            string kasutajaNimi = "";
+            kasutajaNimi = KasutajanimeKüsimine(kasutajaNimi);
+            float eelarve = 0.00f;
+            bool kasKasutajaNõustab = false;
+            Tervitus();
+            kasKasutajaNõustab = NõusolekuKüsimine(kasutajaNimi, kasKasutajaNõustab);
+
+
+        }
+
+        private static void Tervitus()
+        {
+            Console.WriteLine("'OMMIK");
+        }
+
+        private static bool NõusolekuKüsimine(string kasutajaNimi, bool kasKasutajaNõustab)
+        {
+            while (kasKasutajaNõustab == false)
+            {
+                Console.WriteLine($"{kasutajaNimi}, kas see on sinu õige eelarve");
+                Console.WriteLine("Vasta kas jah või ei");
+                string vastus = Console.ReadLine();
+                if (vastus == "jah")
+                {
+                    kasKasutajaNõustab = true;
+                }
+                else
+                {
+                    kasKasutajaNõustab = false;
+                }
+
+
+
+            }
+
+            return kasKasutajaNõustab;
+        }
+
+        private static string KasutajanimeKüsimine(string kasutajaNimi)
+        {
+            while (kasutajaNimi == "")
+            {
+                Console.WriteLine("TERRE; palun sisesta oma kasutajaNimi");
+                kasutajaNimi = Console.ReadLine();
+            }
+            Console.WriteLine("Mis on sinu nädalane eelarve?");
+            return kasutajaNimi;
+        }
+    }
+}
