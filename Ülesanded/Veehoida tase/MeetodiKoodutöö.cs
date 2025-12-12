e700392db1856125a330f492eb16196be268d693
Opilane TTHK <opilane@tthk.ee>
2025-12-12 15:50:44 +0200

    kodutöö

---
+++ Ülesanded/Veehoida tase/MeetodiKoodutöö.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veehoida_tase
+{
+    public class MeetodiKoodutöö
+    {
+        public MeetodiKoodutöö() 
+        { 
+            // ülesanne 1
+            // kirjuta meetod mis tervitab kasutajat 
+            // kirjuta meetod mis ütleb talle headaega 
+            // meetodeid kasutatakse programmis, mis apelsin ananas, kus kõigipealt tsüklis küsitatakse kasutajalt tema nime 
+            // nimi salvestatakse muutujase, muutuja antakse kaasa meetoditele. meetodid ise midagi ei tagasta 
+
+            // ülesanne 2
+            // Kirjuta meetod, mis joonistab käsureale ruudu, võtab parametriks kas täisarvu, (x ja y) ja konsoolivärvi 
+            // Kirjuta teine meetod, mis vaheab konsooli värvi ja võtab parameetriks kasutaja sisestuse 
+            // meetodeid kasutatakse programmis, kus, abrakadabra, küsitakse kasutajalt tema lemmikvärv, teise meetodi abil
+            // teise meetodi tulemus hoitakse muutujas, ning see antakse kaasa esimesele meetodile, mis joonistab ruud aga õige värviga 
+
+            // varasemate ülesannete osi, saab siin taaskasutada 
+
+
+
+
+
+        }
+
+    }
+}
